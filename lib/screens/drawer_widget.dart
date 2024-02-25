c87b40f0d6a5bda6a0ef72bf2065632057dee0aa
Sidhant Kumar <sidhant.loungani@gmail.com>
2024-02-25 23:48:27 +0500

    folder hello

--- lib/screens/drawer_widget.dart
+++ lib/screens/drawer_widget.dart
@@ -27,7 +27,7 @@ class DrawerWidget extends StatelessWidget {
                 ),
                 SizedBox(height: 8),
                 Text(
-                  'Sidhant Kumar ',
+                  'Sidhant Kumar',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 16,
